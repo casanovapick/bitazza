583d1e1d6f9b4099279ea3e1f2d41fbef4c4eae8
Jirac <jirachai.hk@gmail.com>
2020-04-06 22:54:30 +0700

    first commit

---
+++ app/src/main/java/com/assignment/bitazzaassignment/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.assignment.bitazzaassignment
+
+import android.app.Application
+import com.assignment.bitazzaassignment.scarlet.scarletModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(scarletModule)
+        }
+    }
+}
